1c33d3799a176de758e5bd117b30d59985af4d53
benklett <b.klettbach@gmail.com>
2016-06-29 12:16:30 +0200

    Add EventHandling

---
+++ src/exam/ToDoListFrame2.java
@@ -0,0 +1,57 @@
+package exam;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by fricke on 29.06.2016.
+ */
+public class ToDoListFrame2 extends JFrame {
+    protected JButton buttonAdd ;
+    protected JButton buttonDelete ;
+    protected JButton buttonDeleteAll ;
+
+    protected JList<String> listToDo ;
+
+    protected JTextField inputToDo ;
+
+    protected Container content ;
+
+    public ToDoListFrame2(){
+        content = this.getContentPane() ;
+
+        JPanel buttonPanel = new JPanel(new FlowLayout()) ;
+        buttonAdd = new JButton("Hinzufügen") ;
+        buttonAdd.addActionListener(e -> addEntry());
+
+        buttonDelete = new JButton("Erledigt") ;
+        buttonDelete.addActionListener(e -> delete());
+
+        buttonDeleteAll = new JButton("Liste löschen") ;
+        buttonDeleteAll.addActionListener(e -> deleteAll());
+
+        buttonPanel.add(buttonDeleteAll) ;
+        buttonPanel.add(buttonDelete) ;
+        buttonPanel.add(buttonAdd) ;
+
+        listToDo = new JList<>(new DefaultListModel<>()) ;
+
+        inputToDo = new JTextField() ;
+        inputToDo.setText("Aufgabe eingeben...") ;
+    }
+
+    private void deleteAll() {
+        DefaultListModel<String> model = (DefaultListModel<String>)listToDo.getModel();
+        model.removeAllElements();
+    }
+
+    private void delete() {
+        DefaultListModel<String> model = (DefaultListModel<String>)listToDo.getModel();
+        model.remove(listToDo.getSelectedIndex());
+    }
+
+    private void addEntry() {
+        DefaultListModel<String> model = (DefaultListModel<String>)listToDo.getModel();
+        model.add(model.getSize() + 1, inputToDo.getText());
+    }
+}
